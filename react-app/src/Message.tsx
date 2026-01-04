1693bd676194099f8bf93d419e0b3de6df6d8a7a
Komezusenge-Bolice <komezusengebolice@gmail.com>
2026-01-04 12:17:45 +0200

    react updates

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,14 +1,16 @@
-function Button(){
-    return(
-        <button>I'm a button</button>
-    );
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
 }
-export default function MyApp(){
-    return(
-        <div>
-      
-            <h1>Welcome to my app</h1>
-            <Button />
-        </div>
-    );
+
+export default function Message() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
 }
